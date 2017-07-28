9c21ffb8e4fc96a2da61dbeda6e4ad08edbb2686
Onlyprogrammer <mail@example.ru>
2017-07-28 16:18:42 +0300

    new_update

--- file_upload.php
+++ file_upload.php
@@ -1,12 +1,16 @@
 <?php
- //параметры загрузки файла
- $uploaddir = " ";
+ //директория загрузки файла
+ $uploaddir = "/var/www/work.site/vk_bot/download/";
  
  
 
 //загрузка файлов
+foreach($_FILES['files']["name"] as $key => $value){
+  $tmp_name = $_FILES['files']['tmp_name'][$key];
+  $uploaded_file = $uploaddir.$_FILES['files']['name'][$key];
+  move_uploaded_file($tmp_name, $uploaded_file);
+}
 
- //отчёт
- print_r ($_FILES); 
+ 
 
 ?>
